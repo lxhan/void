3b47560526f6f37f4f054f5ce7d7b9c49c089468
lxhan <lxhan@protonmail.com>
2019-11-11 22:05:12 +0900

    add pkg installer

---
+++ pkg.sh
@@ -0,0 +1,44 @@
+sudo xbps-install -Syu &
+
+sudo xbps-install -Sy \
+    # minimal 
+    xorg-minimal \
+    xorg-fonts \
+    xf86-input-synaptics \
+    xf86-input-evdev \
+    xf86-video-intel \
+    alsa-utils \
+    openntpd \
+    simple-mtpfs \
+    base-devel \
+    libXft-devel \
+    libXinerama-devel \
+    wireless_tools \
+    xdg-utils \
+    xprop \
+    xwinwrap \
+    xbacklight \
+    xrdb \
+    xtitle \
+    xclip \
+    tlp \
+    # optional
+    bspwm \
+    sxhkd \
+    polybar \
+    neovim \
+    git \
+    stow \
+    youtube-dl \
+    feh \
+    scrot \
+    mpv \
+    ranger \
+    rxvt-unicode \
+    compton \
+    zsh \
+    rofi \
+    dmenu \
+    firefox \
+    vscode \
+    telegram-desktop
