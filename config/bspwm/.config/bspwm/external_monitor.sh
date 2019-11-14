671d85da3999883dfe14e8c514d241353371318e
lxhan <lxhan@protonmail.com>
2019-11-14 21:51:22 +0900

    bspwm

--- config/bspwm/.config/bspwm/external_monitor.sh
+++ config/bspwm/.config/bspwm/external_monitor.sh
@@ -2,6 +2,7 @@
 
 external_monitor=$(xrandr --query | grep 'HDMI1')
 if [[ $external_monitor == *connected* ]]; then
-    xrandr --output LVDS1 --primary --mode 1366x768 --rotate normal --output HDMI1 --mode 1920x1080 --rotate normal --above LVDS1
+    # xrandr --output LVDS1 --mode 1366x768 --rotate normal --output HDMI1 --primary --mode 1920x1080 --rotate normal --above LVDS1
+    xrandr --output LVDS1 --auto --output HDMI1 --primary --auto --above LVDS1
 fi
 
